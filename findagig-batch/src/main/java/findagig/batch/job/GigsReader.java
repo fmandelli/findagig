16f9cdf86c133194b32f13205bb08a7f3fbe2b17
Diego I. da Costa <diegoicosta@pm.me>
2020-02-27 06:08:19 +0100

    adding logback and logstash dependencies do log structured and one example of use (#15)

--- findagig-batch/src/main/java/findagig/batch/job/GigsReader.java
+++ findagig-batch/src/main/java/findagig/batch/job/GigsReader.java
@@ -2,29 +2,50 @@ package findagig.batch.job;
 
 import findagig.source.driver.SongKickDriver;
 import findagig.source.entity.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
 
 public class GigsReader implements ItemReader<Event> {
 
-    private List<Event> songKickEvents;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private List<Event> songKickEvents = new ArrayList<>();
+    private SongKickDriver driver;
+    private int page = 0;
 
     public GigsReader() {
-        this.songKickEvents = new SongKickDriver().getEventsByLocationFromDate("Winnipeg,", LocalDate.now());;
+        driver = new SongKickDriver();
     }
 
     @Override
     public Event read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
         if (!this.songKickEvents.isEmpty()) {
-            System.out.println("<<<  PROCESSING THE FOLLOWING EVENT FROM SOURCE >>>" + this.songKickEvents.get(0).getId());
+            System.out.println("<<<  PROCESSING THE FOLLOWING EVENT FROM SOURCE >>> " + this.songKickEvents.get(0).getId());
+            return this.songKickEvents.remove(0);
+        } else {
+            page++;
+            songKickEvents = readNewEvents(page);
+            if (songKickEvents.isEmpty()) {
+                page = 0;
+                return null;
+            }
             return this.songKickEvents.remove(0);
         }
-        System.out.println("<<< NO EVENT(S) TO BE PROCESSED >>>");
-        return null;
+    }
+
+    private List<Event> readNewEvents(int fromPage) {
+        long time = System.currentTimeMillis();
+        List<Event> events = driver.getEventsByLocationFromDate("Winnipeg,", LocalDate.now(), fromPage);
+        logger.info("Events page read", keyValue("event", "EVENT_READ"), keyValue("page", fromPage), keyValue("duration", System.currentTimeMillis() - time));
+        return events;
     }
 }
--- findagig-batch/src/test/java/findagig/source/driver/SongkickDriverTest.java
+++ findagig-batch/src/test/java/findagig/source/driver/SongkickDriverTest.java
@@ -1,17 +1,21 @@
 package findagig.source.driver;
 
 import findagig.source.entity.Event;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 class SongkickDriverTest {
 
     @Test
     void getEventsByLocationFromDate() {
         SongKickDriver driver = new SongKickDriver();
-        List<Event> events = driver.getEventsByLocationFromDate("Munchen", LocalDate.now());
+        List<Event> events = driver.getEventsByLocationFromDate("Munchen", LocalDate.now(), 1);
+        assertTrue(events != null);
     }
 
 }
