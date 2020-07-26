079fb904da70809b49d3b95236956336912d39cf
Diego Irismar da Costa <diegoicosta@pm.me>
2020-07-26 16:54:32 +0200

    starting job only when called by the controller

--- findagig-batch/src/main/java/findagig/batch/api/JobController.java
+++ findagig-batch/src/main/java/findagig/batch/api/JobController.java
@@ -1,9 +1,9 @@
 package findagig.batch.api;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +15,24 @@ public class JobController {
     private JobLauncher launcher;
 
     @Autowired
-    private Job job;
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private JobExecutionListener listener;
+
+    @Autowired
+    private Step step;
 
     @RequestMapping("/start")
     public String startJob() throws Exception {
         JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
                 .toJobParameters();
 
-        launcher.run(job, jobParameters);
+        Job job = jobBuilderFactory.get("processJob")
+                .incrementer(new RunIdIncrementer()).listener(listener)
+                .flow(step).end().build();
 
+        launcher.run(job, jobParameters);
         return "Batch job has been invoked";
     }
 }
